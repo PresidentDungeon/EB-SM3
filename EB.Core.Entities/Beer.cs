cf9b7b1b4dd221086ee57a4c8a6a94bab2eaa32c
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2020-11-23 14:28:35 +0100

    Added Basic Entities

---
+++ EB.Core.Entities/Beer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EB.Core.Entities
+{
+    public class Beer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Percentage { get; set; }
+        public double IBU { get; set; }
+        public double EBC { get; set; }
+        public Type Type  { get; set; }
+        public Brand Brand { get; set; }
+        //public Collection Collection { get; set; }
+    }
+}
---
+++ EB.Core.Entities/Type.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EB.Core.Entities
+{
+    public class Type
+    {
+        public int ID { get; set; }
+        public string TypeName { get; set; }
+        public Beer[] Beers { get; set; }
+    }
+}
