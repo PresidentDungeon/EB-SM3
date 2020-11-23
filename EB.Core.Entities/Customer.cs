ba488d5f229360a9bfbfb445d44481f8bfc8bbc7
Jackei94 <55141365+Jackei94@users.noreply.github.com>
2020-11-23 14:28:35 +0100

    Added Basic Entities

---
+++ EB.Core.Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EB.Core.Entities
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string StreetName { get; set; }
+        public int PostalCode { get; set; }
+        public string CityName { get; set; }
+        public Order[] Orders { get; set; }
+    }
+}
---
+++ EB.Core.Entities/Security/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EB.Core.Entities.Security
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] Salt { get; set; }
+        public string UserRole { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
