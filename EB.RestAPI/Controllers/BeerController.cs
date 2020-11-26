51ee78d56758045a0a14951ae47fc2c97b1ba129
Lenschow19 <55918642+Lenschow19@users.noreply.github.com>
2020-11-26 14:06:48 +0100

    All Controllers set up and ready for testing

---
+++ EB.RestAPI/Controllers/BeerController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using EB.Core.ApplicationServices;
+using EB.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EB.RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        #region Dependency Injection
+        private readonly IBeerService BeerService;
+
+        public BeerController(IBeerService beerService)
+        {
+            this.BeerService = beerService;
+        }
+        #endregion
+
+        #region Create
+        // POST api/<BeerController>
+        [HttpPost]
+        [ProducesResponseType(typeof(Beer), 201)]
+        [ProducesResponseType(400)][ProducesResponseType(500)]
+        public ActionResult<Beer> Post([FromBody] Beer beer)
+        {
+            try
+            {
+                return BeerService.CreateBeer(beer);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Read
+        // GET: api/<BeerController>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Beer>), 200)]
+        [ProducesResponseType(404)] [ProducesResponseType(500)]
+        public ActionResult<IEnumerable<Beer>> Get([FromQuery] Filter filter)
+        {
+            try
+            {
+                return Ok(BeerService.GetBeerFilterSearch(filter));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Couldn't load beers. Please try again later.");
+            }
+        }
+
+        // GET api/<BeerController>/5
+        [HttpGet("{ID}")]
+        [ProducesResponseType(typeof(Beer), 200)]
+        [ProducesResponseType(404)] [ProducesResponseType(500)]
+        public ActionResult<Beer> GetByID(int ID)
+        {
+            try
+            {
+                Beer beer = BeerService.GetBeerById(ID);
+                if (beer != null)
+                {
+                    return Ok(beer);
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error loading beer with ID: {ID}\nPlease try again later.");
+            }
+        }
+        #endregion
+
+        #region Update
+        // PUT api/<BeerController>/5
+        [HttpPut("{ID}")]
+        [ProducesResponseType(typeof(Beer), 202)]
+        [ProducesResponseType(400)][ProducesResponseType(404)][ProducesResponseType(500)]
+        public ActionResult<Beer> Put(int id, [FromBody] Beer beer)
+        {
+            if (id < 1 || id != beer.ID)
+            {
+                return BadRequest("Didn't find a matching ID.");
+            }
+
+            return Ok(BeerService.UpdateBeer(beer));
+        }
+        #endregion
+
+        #region Delete
+        // DELETE api/<BeerController>/5
+        [HttpDelete("{ID}")]
+        [ProducesResponseType(typeof(Beer), 202)]
+        [ProducesResponseType(400)][ProducesResponseType(404)][ProducesResponseType(500)]
+        public ActionResult<Beer> Delete(int id)
+        {
+            var beer = BeerService.DeleteBeer(id);
+            if (beer == null)
+            {
+                return StatusCode(404, "Couldn't find beer with ID: " + id);
+            }
+            return Ok($"Beer with ID: {id} has been deleted.");
+        }
+        #endregion
+    }
+}
---
+++ EB.RestAPI/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using EB.Core.ApplicationServices;
+using EB.Core.Entities.Security;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EB.RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        #region Dependency Injection
+        private readonly IUserService UserService;
+
+        public UserController(IUserService userService)
+        {
+            this.UserService = userService;
+        }
+        #endregion
+
+        #region Create
+        // POST api/<BeerController>
+        [HttpPost]
+        [ProducesResponseType(typeof(User), 201)]
+        [ProducesResponseType(400)][ProducesResponseType(500)]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+            try
+            {
+                return UserService.AddUser(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Read
+        // GET api/<BeerController>/5
+        [HttpGet("{ID}")]
+        [ProducesResponseType(typeof(User), 200)]
+        [ProducesResponseType(404)][ProducesResponseType(500)]
+        public ActionResult<User> GetByID(int ID)
+        {
+            try
+            {
+                User user = UserService.GetUserByID(ID);
+                if (user != null)
+                {
+                    return Ok(user);
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error loading User with ID: {ID}\nPlease try again later.");
+            }
+        }
+        #endregion
+
+        #region Update
+        // PUT api/<BeerController>/5
+        [HttpPut("{ID}")]
+        [ProducesResponseType(typeof(User), 202)]
+        [ProducesResponseType(400)][ProducesResponseType(404)][ProducesResponseType(500)]
+        public ActionResult<User> Put(int id, [FromBody] User user)
+        {
+            if (id < 1 || id != user.ID)
+            {
+                return BadRequest("Didn't find a matching ID.");
+            }
+
+            return Ok(UserService.UpdateUser(user));
+        }
+        #endregion
+
+        #region Delete
+        // DELETE api/<BeerController>/5
+        [HttpDelete("{ID}")]
+        [ProducesResponseType(typeof(User), 202)]
+        [ProducesResponseType(400)][ProducesResponseType(404)][ProducesResponseType(500)]
+        public ActionResult<User> Delete(int id)
+        {
+            var user = UserService.DeleteUser(id);
+            if (user == null)
+            {
+                return StatusCode(404, "Couldn't find User with ID: " + id);
+            }
+            return Ok($"User with ID: {id} has been deleted.");
+        }
+        #endregion
+    }
+}
