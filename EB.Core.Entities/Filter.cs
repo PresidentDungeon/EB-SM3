bb486dc6de26134bad1cffe44ebfbc6746d15bbc
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-11-26 10:03:45 +0100

    Added repository for beer and beertype

--- EB.Core.Entities/Filter.cs
+++ EB.Core.Entities/Filter.cs
@@ -8,5 +8,10 @@ namespace EB.Core.Entities
     {
         public int CurrentPage { get; set; }
         public int ItemsPrPage { get; set; }
+
+        public string Name { get; set; }
+        public string SortingType { get; set; }
+        public string Sorting { get; set; }
+        public string ProductType { get; set; }
     }
 }
---
+++ EB.Infrastructure.Data/BeerRepository.cs
@@ -0,0 +1,104 @@
+﻿using EB.Core.DomainServices;
+using EB.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using ProductShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EB.Infrastructure.Data
+{
+    public class BeerRepository : IBeerRepository
+    {
+        private EBContext ctx;
+
+        public BeerRepository(EBContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public Beer AddBeer(Beer beer)
+        {
+            ctx.Attach(beer).State = EntityState.Added;
+            ctx.SaveChanges();
+
+            return beer;
+        }
+
+        public IEnumerable<Beer> ReadBeers()
+        {
+            return ctx.Beers.Include(b => b.Type).Include(b => b.Brand).AsEnumerable();
+        }
+
+        public FilterList<Beer> ReadBeersFilterSearch(Filter filter)
+        {
+            IQueryable<Beer> beers = ctx.Beers.Include(b => b.Type).Include(b => b.Brand).AsQueryable();
+
+            if (!string.IsNullOrEmpty(filter.Name))
+            {
+                beers = from x in beers where x.Name.Contains(filter.Name) select x;
+            }
+            if (!string.IsNullOrEmpty(filter.Sorting) && filter.Sorting.ToLower().Equals("asc"))
+            {
+                if (filter.SortingType.Equals("IBU")) { beers = from x in beers orderby x.IBU select x; }
+                if (filter.SortingType.Equals("EBC")) { beers = from x in beers orderby x.EBC select x; }
+                if (filter.SortingType.Equals("ALF")) { beers = from x in beers orderby x.Name select x; }
+            }
+            else if (!string.IsNullOrEmpty(filter.Sorting) && filter.Sorting.ToLower().Equals("desc"))
+            {
+                if (filter.SortingType.Equals("IBU")) { beers = from x in beers orderby x.IBU descending select x; }
+                if (filter.SortingType.Equals("EBC")) { beers = from x in beers orderby x.EBC descending select x; }
+                if (filter.SortingType.Equals("ALF")) { beers = from x in beers orderby x.Name descending select x; }
+                if (filter.SortingType.Equals("ADDED")) { beers = from x in beers orderby x descending select x; }
+            }
+
+            int totalItems = beers.Count();
+
+            if (filter.CurrentPage > 0)
+            {
+                beers = beers.Skip((filter.CurrentPage - 1) * filter.ItemsPrPage).Take(filter.ItemsPrPage);
+                if (beers.Count() == 0 && filter.CurrentPage > 1)
+                {
+                    throw new InvalidDataException("Index out of bounds");
+                }
+            }
+
+            FilterList<Beer> filterList = new FilterList<Beer> { totalItems = totalItems, List = beers.ToList() };
+
+            return filterList;
+        }
+
+        public Beer ReadBeerById(int id)
+        {
+            return ctx.Beers.Include(beers => beers.Type).Include(beer => beer.Brand).FirstOrDefault(x => x.ID == id);
+        }
+
+        public Beer UpdateBeerInRepo(Beer beerUpdate)
+        {
+            ctx.Attach(beerUpdate).State = EntityState.Modified;
+            ctx.Entry(beerUpdate).Reference(product => product.Type).IsModified = true;
+            ctx.Entry(beerUpdate).Reference(product => product.Brand).IsModified = true;
+            ctx.SaveChanges();
+
+            return ReadBeerById(beerUpdate.ID);
+        }
+
+
+        public Beer DeleteBeerInRepo(int id)
+        {
+            var removedBeer = ctx.Beers.Remove(ReadBeerById(id));
+            ctx.SaveChanges();
+            return removedBeer.Entity;
+        }
+
+
+
+
+
+
+
+
+    }
+}
